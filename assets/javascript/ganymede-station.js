885bbf8408ad63c0a72299b8d913a0ed99edf143
dbstocker <dstocker@notesettersinc.com>
2018-09-20 00:08:54 -0400

    almost there

--- assets/javascript/ganymede-station.js
+++ assets/javascript/ganymede-station.js
@@ -20,10 +20,27 @@ var vFrequency = "";
 var nArrival = "";
 var tRemaining = "";
 
-$("#submit").on("click", function () {
+// Using Moment.js to set up scheduler
 
+// construct time display
+var time = moment();
+var currentMonth = moment(time).format("MMMM");
+var currentWeekDay = moment(time).format("dddd");
+var currentDay = moment(time).format("Do");
+var currentYear = moment(time).format("YYYY");
+var expanseYear = parseInt(currentYear) + 340;
+var expanseDate = currentWeekDay + ", " + currentMonth + " " + currentDay + " " + expanseYear;
+var currentTime = moment(time).format("h:mm:ss a");
+var expanseDateTime = expanseDate + ", " + currentTime;
+
+console.log("Current Date: " + expanseDateTime);
+console.log("Current Time: " + currentTime);
+
+$("#time-clock").text(expanseDateTime);
+
+$("#submit").on("click", function() {
   // clear form
-  function clearForm(){
+  function clearForm() {
     $("#v-name").val("");
     $("#v-destination").val("");
     $("#v-first-arrival").val("");
@@ -31,16 +48,24 @@ $("#submit").on("click", function () {
   }
 
   // store inputs
-  vessel = $("#v-name").val().trim();
-  vDestination = $("#v-destination").val().trim();
-  vFirstArrival = $("#v-first-arrival").val().trim();
-  vFrequency = $("#v-frequency").val().trim();
+  vessel = $("#v-name")
+    .val()
+    .trim();
+  vDestination = $("#v-destination")
+    .val()
+    .trim();
+  vFirstArrival = $("#v-first-arrival")
+    .val()
+    .trim();
+  vFrequency = $("#v-frequency")
+    .val()
+    .trim();
   // nArrival = vFirstArrival + vFrequency; I'll need moment.js to do these calculations
 
-  console.log(vessel);
-  console.log(vDestination);
-  console.log(vFirstArrival);
-  console.log(vFrequency);
+  console.log("New Vessel Name: " + vessel);
+  console.log("New Destination: " + vDestination);
+  console.log("New First Arrival: " + vFirstArrival);
+  console.log("New Frequency: " + vFrequency);
 
   // put input values into database
   database.ref().push({
@@ -50,4 +75,27 @@ $("#submit").on("click", function () {
     frequency: vFrequency
   });
   clearForm();
+});
+
+// display existing data in schdule display
+database.ref().on("child_added", function(snapshot) {
+  var entry = snapshot.val();
+
+  console.log(entry.vessel);
+  console.log(entry.destination);
+  console.log(entry.firstArrival);
+  console.log(entry.frequency);
+
+  var nRow = $("<tr>");
+  var nCell = $(
+    `<td>${entry.vessel}</td>
+     <td>${entry.destination}</td>
+     <td>${entry.firstArrival}</td>
+     <td>${entry.frequency}</td>
+     <td>To be determinde (moment.js)</td>
+     `
+  );
+
+  $("#entries").append(nRow);
+  $(nRow).append(nCell);
 });
